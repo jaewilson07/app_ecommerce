a038ead49619c93d8b5daa11c084da4626a081ed
Jae Wilson <jaewilson07@gmail.com>
2020-08-11 17:08:20 +0200

    Updates

---
+++ precache-manifest.340163095f7a38dc677e9e29a3501fa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2473c9581bf7cd1fc10100676b441b09",
+    "url": "/app_ecommerce/index.html"
+  },
+  {
+    "revision": "7af72a0af8d7e179edbb",
+    "url": "/app_ecommerce/static/css/main.11d8bcb8.chunk.css"
+  },
+  {
+    "revision": "2ba0784b9abe929fe2d5",
+    "url": "/app_ecommerce/static/js/2.0e743fb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app_ecommerce/static/js/2.0e743fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7af72a0af8d7e179edbb",
+    "url": "/app_ecommerce/static/js/main.e98fdf8c.chunk.js"
+  },
+  {
+    "revision": "61e56ffa57f5ccbcb9fc",
+    "url": "/app_ecommerce/static/js/runtime-main.b80096e4.js"
+  }
+]);
